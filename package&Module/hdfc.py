6736cc76838d04a20aaac23ba9f416e0579d6179
aimartians2 <aimartians2@gmail.com>
2023-08-29 10:30:24 +0530

    commit

---
+++ package&Module/hdfc.py
@@ -0,0 +1,23 @@
+# import icici
+
+# icici.change_pin()
+# icici.deposit()
+# icici.withdrawal()
+# icici.fund_transfer()
+
+# from icici import *
+# change_pin()
+# deposit()
+# withdrawal()
+# fund_transfer()
+
+# from icici import  deposit,withdrawal
+# deposit()
+# withdrawal()
+
+
+# from icici import deposit as d
+# d()
+
+import krishna.icici as deepak
+deepak.withdrawal()
---
+++ package&Module/krishna/icici.py
@@ -0,0 +1,11 @@
+def deposit():
+    print("Welcome to deposite section")
+
+def withdrawal():
+    print("Welcome to withdrawal section")
+
+def change_pin():
+    print("Welcome to change pin")
+
+def fund_transfer():
+    print("Welcome to fund transfer")
